e8280d2c482831129d7eaac377eeab40c959b2aa
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-11-08 21:02:43 -0800

    Basic design of Profile page is established

--- client/src/features/User/index.jsx
+++ client/src/features/User/index.jsx
@@ -1,10 +1,21 @@
-
-import React from 'react'
+import { Container, Box, Typography } from "@mui/material";
+import WidgetWrapper from "components/WidgetWrapper";
+import React from "react";
 
 const ProfilePage = () => {
-  return (
-    <div>ProfilePage</div>
-  )
-}
+  const pathname = window.location.pathname;
+  const parts = pathname.split("/");
+  const username = parts[parts.length - 1];
 
-export default ProfilePage
+  return (
+    <Container maxWidth="md">
+      <WidgetWrapper sx={{ marginTop: 5 }}>
+        <Box>
+          <Typography>This is Profile Page of {username}</Typography>
+        </Box>
+      </WidgetWrapper>
+    </Container>
+  );
+};
+
+export default ProfilePage;
