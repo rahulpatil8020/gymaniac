30fc5fb260426226e8bd58d3a31d4c4966955674
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-11-08 21:00:49 -0800

    Link to profile page added to the complete profile button

--- client/src/features/Home/AISuggestionsWidget.jsx
+++ client/src/features/Home/AISuggestionsWidget.jsx
@@ -1,7 +1,9 @@
 import WidgetWrapper from "components/WidgetWrapper";
 import React from "react";
 import { Stack, Divider, Typography, Button } from "@mui/material";
-const AISuggestionsWidget = () => {
+import { useNavigate } from "react-router-dom";
+const AISuggestionsWidget = ({ userInfo }) => {
+  const navigate = useNavigate();
   return (
     <WidgetWrapper>
       <Stack
@@ -16,7 +18,14 @@ const AISuggestionsWidget = () => {
           Atlas is an AI program that you can use to create personalized workout
           goals and suggestions. Complete your profile to use this feature.
         </Typography>
-        <Button>Complete Profile</Button>
+
+        <Button
+          onClick={() => {
+            navigate(`/profile/${userInfo?.username}`);
+          }}
+        >
+          Complete Profile
+        </Button>
       </Stack>
     </WidgetWrapper>
   );
